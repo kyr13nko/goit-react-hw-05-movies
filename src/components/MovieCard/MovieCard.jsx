d7e51e9f7540bf32251d9703cecc93f6397b4361
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-09-29 23:36:45 +0300

    movie card test image

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,16 +1,44 @@
+import { CardContainer, Image } from './MovieCard.styled';
+
+const noPoster = '../../images/no-cover.png';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+
 const MovieCard = ({ movieDetails }) => {
   const {
-    // poster_path,
+    poster_path,
+    backdrop_path,
     title,
-    // vote_average,
-    // overview,
-    // genres,
     name,
+    // vote_average,
+    // release_date,
+    overview,
+    genres,
   } = movieDetails;
+
+  const poster =
+    poster_path || backdrop_path
+      ? IMG_PATH + (poster_path ?? backdrop_path)
+      : noPoster;
+
+  console.log('movieDetails', movieDetails);
 
   return (
     <>
-      <h1>{name || title}</h1>
+      <button type="button">Go back</button>
+      <CardContainer>
+        <Image src={poster} alt={name || title} />
+        <div>
+          <h1>{name || title}</h1>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          <ul>
+            {genres?.map(({ id, name }) => {
+              return <li key={id}>{name}</li>;
+            })}
+          </ul>
+        </div>
+      </CardContainer>
     </>
   );
 };
---
+++ src/components/MovieCard/MovieCard.styled.js
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const CardContainer = styled.div`
+  display: flex;
+  gap: 24px;
+
+  margin-top: 10px;
+`;
+
+export const Image = styled.img`
+  max-width: 300px;
+`;
