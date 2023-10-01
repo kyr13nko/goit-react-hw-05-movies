709a71f84844355151f76bc566ee10e2f6b89bb3
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-10-02 02:32:01 +0300

    add header styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,26 @@
 import { Outlet } from 'react-router-dom';
-import { Container, Header, Navigation, StyledLink } from './Layout.styled';
+import {
+  Container,
+  Header,
+  Logo,
+  Navigation,
+  StyledLink,
+} from './Layout.styled';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Suspense } from 'react';
 
+import { ReactComponent as LogoIcon } from '../../icons/movie-search.svg';
+
 const Layout = () => {
   return (
     <>
       <Header>
+        <Logo>
+          <LogoIcon width={48} height={48} />
+          <b>Movies Search Site</b>
+        </Logo>
         <Navigation>
           <StyledLink to="/" end>
             Home
--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -8,11 +8,20 @@ export const Header = styled.header`
   z-index: 100;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 12px 24px;
   color: #ffffff;
   background-color: #ffffff;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  font-size: 24px;
+  color: #000000;
 `;
 
 export const Navigation = styled.nav`
